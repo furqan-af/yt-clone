16a10199b4d675aed1b1cb5f6d9cdcd3c7841bfb
furqan <furqancsit7@gmail.com>
2024-08-03 23:13:01 +0530

    Create model to track user likes for videos, tweets, and comments.

---
+++ backend/models/like.model.js
@@ -0,0 +1,24 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const likeSchema = new Schema({
+
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    },
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+    },
+    tweet: {
+        type: Schema.Types.ObjectId,
+        ref: "Tweet"
+    },
+    likedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    
+},{timestamps:true})
+
+const Like = model("Like", likeSchema)
